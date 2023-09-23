234b71cd2080efe59663802480bfdbdf3f4d4567
Samuel Barnett <samuel.barnett@mymail.champlain.edu>
2023-09-23 15:36:33 -0400

    Finished Assignment 3

--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,4 +1,27 @@
 // add your imports here
+#include <iostream>
+using namespace std;
+
+
 int main(){
   // code here
+  unsigned int seed, numberOfResults;
+  signed int minRange, maxRange;
+
+  // input------------------------------------------------------------------------------------------
+  cin >> seed >> numberOfResults >> minRange >> maxRange;
+
+  for (int i = 0; i < numberOfResults; i++)
+  {
+    // calculate seed-------------------------------------------------------------------------------
+    seed ^= seed << 13;
+    seed ^= seed >> 17;
+    seed ^= seed << 5;
+
+
+    // final result---------------------------------------------------------------------------------
+    int final = minRange + (seed % (maxRange - minRange + 1));
+    cout << final << endl;
+  }
+
 }
